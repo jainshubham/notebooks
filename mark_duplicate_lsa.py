0911499132e85bf2a2ac9f1e886f65d0d7bfcfc0
vagrant <vagrant@localhost.localdomain>
2015-01-30 08:20:47 +0000

    corrected access to clusters using dict comprehensions

--- mark_duplicate_lsa.py
+++ mark_duplicate_lsa.py
@@ -23,13 +23,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         from entry.models import Entry
-
+        start_date = datetime.now()
+        end_date_comparision = start_date - timedelta(hours=250)
         t0= time()
-        item = Entry.objects.order_by("id")[:1000]  # Note: Can't filter by approved on or approved_by as this is being modified in the code
-        Entry.objects.update(duplicate_of=None)
-        #storySoup = item.values_list('body_html', flat=True) 
-        storyCols = item.values_list('title', 'title', 'title', 'body_html')        
-        storySoup = [' '.join(cols) for cols in storyCols] 
+        item = Entry.objects.order_by("id").filter(created_on__gte=end_date_comparision, created_on__lte=start_date)
+        item.update(duplicate_of=None)
+        storyCols = item.values_list('title', 'title', 'title', 'body_html')
+        storySoup = [' '.join(cols) for cols in storyCols]
 
         #Vectorizer
         hasher = HashingVectorizer(stop_words='english', non_negative=True,
@@ -48,47 +48,44 @@ class Command(BaseCommand):
 
 
         # Do the actual clustering
-        nc = int(len(storySoup)*.95)
+        nc = int(len(storySoup)*0.95)
         km = MiniBatchKMeans(n_clusters= nc, max_iter=100, n_init=5, compute_labels=True, init_size=int(nc*3), batch_size=int(nc*.02), reassignment_ratio=.1, verbose=False).fit(X)
         d = km.transform(X)
         #km = KMeans(n_clusters=nc, init='k-means++', max_iter=100, n_init=10, verbose=True).fit(X)
         print('\x1b[1;31m'+"Stories Clustered in  in %fs" % (time() - t0)+'\x1b[0m')
-        
-        
-        # Finding root and autotagging
-        for k in np.unique(km.labels_):
-            if(not math.isnan(k)):
-                members = np.where(km.labels_ == k)
-            if k == -1:
-                #print("outliers:")
-                continue    
-            else:
-                pass
+
+        #creating a dict iterator form the cluster
+        clusteredDict = {}
+        for l in range(len(km.labels_)):
+            #item[l].score = score
+            try:
+                clusteredDict[km.labels_[l]].append(item[l])
+            except:
+                clusteredDict.setdefault(km.labels_[l], [item[l]])
+
+        #Finding root and autotagging
+        for cluster in clusteredDict.values():
+            #print("\n")
             largest = 0
-            root=None
-            print("\n")
-            for it in members[0]:
-                score = 0
-                currItem = item[it]
-                score += (((datetime.now() - currItem.created_on).total_seconds() // 3600)-190)/24 # Weight for story age
-                score += min(d[it])     # score from clustering
-                currItem.score = score
-                if(len(members[0])>1 and len(members[0])<5):
-                    print(currItem.title)
+            for story in cluster:
+                score = (datetime.now() - story.created_on).total_seconds()      # Weight for story age
+                #score += min(d[it])     # score from clustering
+                #story.score = score
+                #if(len(cluster)>1 and len(cluster)<5):
+                #   print(story.title)
                 if(score>largest):
                         largest=score
-                        root=currItem
-            for it in members[0]:
-                currItem = item[it]
-                if (currItem.id!=root.id):
-                    currItem.duplicate_of=root
-                    currItem.status = root.status
-                    currItem.priority = root.priority
-                    if (currItem.approved_by_id is None or currItem.approved_by_id == settings.SYSTEM_ADMIN_USER_ID) and currItem.status in [2, -1, 5, 6]:
-                        currItem.approved_on = datetime.now()
-                        currItem.approved_by_id = settings.SYSTEM_ADMIN_USER_ID
-                    currItem.primaryl_topic_id = root.primary_topic_id
-                    currItem.primary_industry_id = root.primary_industry_id
-                    currItem.copy_tags_and_sentiment_from(root, copyOnlySemanticTags=True)
-                    currItem.save(doNotSaveDuplicateChildren=True)
+                        root=story
+            for story in cluster:
+                if (story.id!=root.id):
+                    story.duplicate_of=root
+                    story.status = root.status
+                    story.priority = root.priority
+                    if (story.approved_by_id is None) and story.status in [2, -1, 5, 6]:
+                        story.approved_on = datetime.now()
+                        story.approved_by_id = settings.SYSTEM_ADMIN_USER_ID
+                    story.primary_topic_id = root.primary_topic_id
+                    story.primary_industry_id = root.primary_industry_id
+                    story.copy_tags_and_sentiment_from(root, copyOnlySemanticTags=True)
+                    story.save(doNotSaveDuplicateChildren=True)
         print('\x1b[1;31m'+"Overall time taken %fs" % (time() - t0)+'\x1b[0m')
