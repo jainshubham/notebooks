f09ddea782012dcf26ee79dcd9491b4e7b689cb8
Shubham Jain <shubham2006@gmail.com>
2015-02-12 21:01:04 +0530

    Optimzed queryset handeling

--- mark_duplicate_smx.py
+++ mark_duplicate_smx.py
@@ -24,13 +24,14 @@ class Command(BaseCommand):
         end_date_comparision = start_date - timedelta(hours=48)
         t0= time()
         
-        item = Entry.objects.order_by("approved_on")
-        itemList = item[:500]
+        items = Entry.objects.order_by("approved_on").values_list('title', 'body_html', 'id', 'created_on')
+        itemList = list(items[:2000])
         size = len(itemList)
-        print(size)
-        storyCols = itemList.values_list('title', 'body_html')
-        storyBody = [''.join(cols[1]) for cols in storyCols]
-        storyTitle = [''.join(cols[0]) for cols in storyCols]
+        storyId = [(cols[2]) for cols in itemList]
+        storyBody = [cols[1] for cols in itemList]
+        storyTitle = [(cols[0]) for cols in itemList]
+        storyCreated = [(cols[3]) for cols in itemList]
+
 
         hasher = TfidfVectorizer(stop_words='english', token_pattern=u'(?u)[a-zA-Z0-9]+',  ngram_range = (1,3), max_df=.99)
         X_body = hasher.fit_transform(storyBody)
@@ -52,23 +53,20 @@ class Command(BaseCommand):
         document_distances_lsa_body = (X * X.T)
         
         print('\x1b[1;31m'+"ML part completed in %fs" % (time() - t0)+'\x1b[0m')
+        
         """
-        document_distances = np.maximum(document_distances_title.toarray(), document_distances_body.toarray(), document_distances_lsa_body.toarray())
         nc = int(size*.49)
         km = MiniBatchKMeans(n_clusters= nc, max_iter=100, n_init=5, compute_labels=True, init_size=int(nc*3), batch_size=int(nc*.02), reassignment_ratio=.9, verbose=False).fit(X)
         clusterList = km.labels_
         """
         
-        document_distances = np.maximum(document_distances_title.toarray(), document_distances_body.toarray(), document_distances_lsa_body.toarray())
-
         clusterList = [None] * size
         scoreList = [0] * size
         print(size)
         for i in range(size):
             count = 1
             for j in xrange(i,size):
-                s = document_distances[i,j]
-                scoreList[i] += document_distances[i,j] 
+                scoreList[i] += document_distances_lsa_body[i,j] 
                 if((i!=j) and 
                 (document_distances_title[i,j]==0 and document_distances_body[i,j]>.70) and 
                 (document_distances_title[i,j]>0.4 or document_distances_body[i,j]>.42 or document_distances_lsa_body[i,j]>0.999)):
@@ -78,19 +76,15 @@ class Command(BaseCommand):
                     clusterList[j] = j 
         print('\x1b[1;31m'+"Cluster List calculated in %fs" % (time() - t0)+'\x1b[0m')
                 
-        
-
-        print('\x1b[1;31m'+"Cluster List calculated in %fs" % (time() - t0)+'\x1b[0m')
-
         #creating a dict iterator form the cluster
         clusteredDict = {}
         scoreDict = {}
         print(len(clusterList))
         for l in range(len(clusterList)):
             try:
-                clusteredDict[clusterList[l]].append([itemList[l],scoreList[l]])
+                clusteredDict[clusterList[l]].append([storyId[l],scoreList[l], storyCreated[l]])
             except:
-                clusteredDict.setdefault(clusterList[l], [[itemList[l], scoreList[l]]])
+                clusteredDict.setdefault(clusterList[l], [[storyId[l], scoreList[l], storyCreated[l]]])      
         print('\x1b[1;31m'+"Cluster Dict in %fs" % (time() - t0)+'\x1b[0m')
 
 
@@ -99,22 +93,22 @@ class Command(BaseCommand):
             if(len(cluster)>1):
                 largest = 0
                 for story in cluster:
-                    score = (datetime.now() - story[0].created_on).total_seconds()
+                    score = (datetime.now() - story[2]).total_seconds()
                     score = story[1]
-                    #print(story[0].title, score)
                     if(score>largest):
                             largest=score
                             root=story[0]
+                rootStory = Entry.objects.filter(id=root)
                 for story in cluster:
-                    if (story[0].id!=root.id):
-                        story[0].duplicate_of=root
-                        story[0].status = root.status
-                        story[0].priority = root.priority
-                        if (story[0].approved_by_id is None) and story[0].status in [2, -1, 5, 6]:
-                            story[0].approved_on = datetime.now()
-                            story[0].approved_by_id = settings.SYSTEM_ADMIN_USER_ID
-                        story[0].primary_topic_id = root.primary_topic_id
-                        story[0].primary_industry_id = root.primary_industry_id
-                        story[0].copy_tags_and_sentiment_from(root, copyOnlySemanticTags=True)
-                        story[0].save(doNotSaveDuplicateChildren=True)
+                        duplicate = Entry.objects.filter(id=root)
+                        duplicate[0].duplicate_of=rootStory[0]
+                        duplicate[0].status = rootStory[0].status
+                        duplicate[0].priority = rootStory[0].priority
+                        if (duplicate[0].approved_by_id is None) and duplicate[0].status in [2, -1, 5, 6]:
+                            duplicate[0].approved_on = datetime.now()
+                            duplicate[0].approved_by_id = settings.SYSTEM_ADMIN_USER_ID
+                        duplicate[0].primary_topic_id = rootStory[0].primary_topic_id
+                        duplicate[0].primary_industry_id = rootStory[0].primary_industry_id
+                        duplicate[0].copy_tags_and_sentiment_from(rootStory[0], copyOnlySemanticTags=True)
+                        duplicate[0].save(doNotSaveDuplicateChildren=True)
         print('\x1b[1;31m'+"Overall time taken %fs" % (time() - t0)+'\x1b[0m')
