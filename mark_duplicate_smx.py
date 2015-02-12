49613be2b9fe1883fcc362a099fdc46df97bd586
Shubham Jain <shubham2006@gmail.com>
2015-02-12 21:27:05 +0530

    Score added

--- mark_duplicate_smx.py
+++ mark_duplicate_smx.py
@@ -19,11 +19,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         from entry.models import Entry
         similarityThreshold = 0.465
-        
+
         start_date = datetime.now()
         end_date_comparision = start_date - timedelta(hours=48)
         t0= time()
-        
+
         items = Entry.objects.order_by("approved_on").values_list('title', 'body_html', 'id', 'created_on')
         itemList = list(items[:2000])
         size = len(itemList)
@@ -35,7 +35,7 @@ class Command(BaseCommand):
 
         hasher = TfidfVectorizer(stop_words='english', token_pattern=u'(?u)[a-zA-Z0-9]+',  ngram_range = (1,3), max_df=.99)
         X_body = hasher.fit_transform(storyBody)
-        
+
         thasher = TfidfVectorizer(stop_words="english", token_pattern=u'(?u)[a-zA-Z0-9]+', ngram_range = (1,3), max_df=0.005)
         X_title = thasher.fit_transform(storyTitle)
 
@@ -45,46 +45,47 @@ class Command(BaseCommand):
 
         X = csr_matrix(X_title)
         document_distances_title = (X * X.T)
-        
+
         X = csr_matrix(X_body)
         document_distances_body = (X * X.T)
-        
+
         X = csr_matrix(X_lsa_body)
         document_distances_lsa_body = (X * X.T)
-        
+
         print('\x1b[1;31m'+"ML part completed in %fs" % (time() - t0)+'\x1b[0m')
-        
+
         """
         nc = int(size*.49)
         km = MiniBatchKMeans(n_clusters= nc, max_iter=100, n_init=5, compute_labels=True, init_size=int(nc*3), batch_size=int(nc*.02), reassignment_ratio=.9, verbose=False).fit(X)
         clusterList = km.labels_
         """
-        
+
         clusterList = [None] * size
         scoreList = [0] * size
+
+
         print(size)
         for i in range(size):
             count = 1
             for j in xrange(i,size):
-                scoreList[i] += document_distances_lsa_body[i,j] 
-                if((i!=j) and 
-                (document_distances_title[i,j]==0 and document_distances_body[i,j]>.70) and 
+                scoreList[i] += document_distances_lsa_body[i,j] + ((datetime.now() - storyCreated[j]).total_seconds())/(3600*48)
+                if((i!=j) and
+                (document_distances_title[i,j]==0 and document_distances_body[i,j]>.70) and
                 (document_distances_title[i,j]>0.4 or document_distances_body[i,j]>.42 or document_distances_lsa_body[i,j]>0.999)):
                             clusterList[j] = i
-                            count = count + 1 
+                            count = count + 1
                 if(clusterList[j] == None):
-                    clusterList[j] = j 
+                    clusterList[j] = j
         print('\x1b[1;31m'+"Cluster List calculated in %fs" % (time() - t0)+'\x1b[0m')
-                
+
         #creating a dict iterator form the cluster
         clusteredDict = {}
-        scoreDict = {}
         print(len(clusterList))
         for l in range(len(clusterList)):
             try:
                 clusteredDict[clusterList[l]].append([storyId[l],scoreList[l], storyCreated[l]])
             except:
-                clusteredDict.setdefault(clusterList[l], [[storyId[l], scoreList[l], storyCreated[l]]])      
+                clusteredDict.setdefault(clusterList[l], [[storyId[l], scoreList[l], storyCreated[l]]])
         print('\x1b[1;31m'+"Cluster Dict in %fs" % (time() - t0)+'\x1b[0m')
 
 
@@ -93,7 +94,6 @@ class Command(BaseCommand):
             if(len(cluster)>1):
                 largest = 0
                 for story in cluster:
-                    score = (datetime.now() - story[2]).total_seconds()
                     score = story[1]
                     if(score>largest):
                             largest=score
